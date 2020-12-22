3b3ecc9c827ca33ed1cabafde0d4fcfb857f4691
langliang3333 <75779508+langliang3333@users.noreply.github.com>
2020-12-22 20:24:13 +0800

    开启ssrplus 添加微信推送 京东签到

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,17 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Uncomment a feed source 开启ssrplus
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#添加adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+#添加serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+#添加luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
